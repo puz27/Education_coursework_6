aa403eff6bb90aab748e044fddd94f015d994d03
anv <anv@woori.ru>
2023-07-07 17:17:00 +0300

    end

--- users/management/commands/cron_send.py
+++ users/management/commands/cron_send.py
@@ -1,10 +1,19 @@
 from crontab import CronTab
-from django_crontab.crontab import Crontab
+from mailing.services import sendmail_after
+from datetime import datetime
+import pycron
 
 
-def set_cron():
-    with CronTab(user=True) as cron:
-        job = cron.new(command='echo hello_world')
-        job.minute.every(1)
-    print('cron.write() was just executed')
+# def set_cron():
+#     with CronTab(user=True) as cron:
+#         job = cron.new(command=sendmail_after)
+#         job.minute.every(1)
+#     print('cron.write() was just executed')
 
+
+# @pycron.cron("* * * * * ")
+# async def test(timestamp: datetime):
+#     print(f"test cron job running at {timestamp}")
+#
+# if __name__ == '__main__':
+#     pycron.start()
